0c49792d141f15403f32b7eff7e6895592efc6dd
print-flavio <flavio@vghcorp.com>
2023-08-17 04:10:57 -0700

    Complete exercise

---
+++ Instantiate.py
@@ -0,0 +1,32 @@
+#Step 1
+class MyFirstClass:
+    def __init__(self):
+        print("Who wrote this?")
+
+#Step 2 is not necessary.
+        index = "Author-Book" 
+
+#Step 3
+    def hand_list(self, philosopher, book):
+         
+#Step 4         
+         print(philosopher + " wrote the book: " + book)
+
+#Step 5
+step_four_2 = MyFirstClass()
+step_four_2.hand_list("Plato", "Republic")
+whodunnit = MyFirstClass()
+whodunnit.hand_list("Sun Tzu", "The Art of War")
+
+        
+    
+
+
+
+
+
+
+
+
+
+
